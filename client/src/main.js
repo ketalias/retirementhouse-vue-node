e7b501f68c7ec6e207cc7c2ae0ed39d800bfb75c
Roman <kovachroman0704@gmail.com>
2025-10-19 17:46:12 +0300

    hotfix babel

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp, nextTick } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
@@ -10,39 +11,43 @@ import './assets/tailwind.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-import { createI18n } from 'vue-i18n'
+async function initializeApp() {
+  const app = createApp(App)
 
-const app = createApp(App)
+  app.use(createPinia())
+  app.use(router)
 
-app.use(createPinia())
-app.use(router)
+  const userLocale = localStorage.getItem('user-locale') || 'uk'
+  
+  const i18n = createI18n({
+    legacy: false,
+    locale: userLocale,
+    fallbackLocale: 'uk',
+    messages: {
+      uk: (await import('./locales/uk.json')).default,
+      en: (await import('./locales/en.json')).default,
+      hu: (await import('./locales/hu.json')).default,
+    },
+  })
+  
+  app.use(i18n)
 
-const userLocale = localStorage.getItem('user-locale')
-const i18n = createI18n({
-  legacy: false,
-  locale: userLocale || 'uk',
-  fallbackLocale: 'uk',
-  messages: {
-    uk: (await import('./locales/uk.json')).default,
-    en: (await import('./locales/en.json')).default,
-    hu: (await import('./locales/hu.json')).default,
-  },
-})
-app.use(i18n)
+  app.mount('#app')
 
-app.mount('#app')
+  AOS.init({
+    duration: 800,
+    easing: 'ease-out',
+    once: true,
+    offset: 100,
+  })
 
-AOS.init({
-  duration: 800,
-  easing: 'ease-out',
-  once: true,
-  offset: 100,
-})
+  router.afterEach(() => {
+    setTimeout(() => {
+      nextTick(() => {
+        AOS.refresh()
+      })
+    }, 600)
+  })
+}
 
-router.afterEach(() => {
-  setTimeout(() => {
-    nextTick(() => {
-      AOS.refresh()
-    })
-  }, 600)
-})
+initializeApp()
